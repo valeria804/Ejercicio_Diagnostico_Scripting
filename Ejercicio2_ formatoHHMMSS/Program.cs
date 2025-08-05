96c9dd7002e231ec712128667bb785fa6c724b8b
valeria804 <valeria.blanquicett@upb.edu.co>
2025-08-04 22:24:32 -0500

    hice actividad 2

---
+++ Ejercicio2_ formatoHHMMSS/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Ejercicio2__formatoHHMMSS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese una cantidad de segundos:");
+            int segundos = int.Parse(Console.ReadLine());
+
+            string tiempoFormateado = ConvertirASegundos(segundos);
+            Console.WriteLine("Formato HH:MM:SS = " + tiempoFormateado);
+        }
+
+        static string ConvertirASegundos(int totalSegundos)
+        {
+            int horas = totalSegundos / 3600;
+            int minutos = (totalSegundos % 3600) / 60;
+            int segundos = totalSegundos % 60;
+
+            return $"{horas:D2}:{minutos:D2}:{segundos:D2}";
+        }
+    }
+}
